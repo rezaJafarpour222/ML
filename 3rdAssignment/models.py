04594f00df0fd855d69e8a3aaf1c72e86475a39c
Reza <rezajafarpoor222@gmail.com>
2026-01-25 18:32:11 +0330

    models finished

---
+++ 3rdAssignment/models.py
@@ -0,0 +1,116 @@
+from configparser import MAX_INTERPOLATION_DEPTH
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+
+from Perceptron.metrics import accuracy, f1_measure, loss_calculator, precision, recall
+from utils import splitter
+
+RANDOM = 42
+
+
+def SVM(data, label, splitPercentage):
+    (X_train, Y_train, X_test, Y_test) = splitter(
+        data=data, label=label, splitPrecent=splitPercentage
+    )
+    model = SVC(kernel="linear")
+    model.fit(X_train, Y_train)
+    train_pred = model.predict(X_train)
+    test_pred = model.predict(X_test)
+    (train_TP, train_TN, train_FP, train_FN) = loss_calculator(train_pred, Y_train)
+    (test_TP, test_TN, test_FP, test_FN) = loss_calculator(test_pred, Y_test)
+
+    train_acc = accuracy(TP=train_TP, TN=train_TN, FP=train_FP, FN=train_FN)
+    train_recall = recall(TP=train_TP, FN=train_FN)
+    train_precision = precision(TP=train_TP, FP=train_FP)
+    train_f1_measure = f1_measure(precision=train_precision, recall=train_recall)
+
+    test_acc = accuracy(TP=test_TP, TN=test_TN, FP=test_FP, FN=test_FN)
+
+    test_recall = recall(TP=test_TP, FN=test_FN)
+    test_precision = precision(TP=test_TP, FP=test_FP)
+    test_f1_measure = f1_measure(precision=test_precision, recall=test_recall)
+
+    return (
+        train_acc,
+        test_acc,
+        train_recall,
+        test_recall,
+        train_precision,
+        test_precision,
+        train_f1_measure,
+        test_f1_measure,
+    )
+
+
+def LDA(data, label, splitPercentage):
+    (X_train, Y_train, X_test, Y_test) = splitter(
+        data=data, label=label, splitPrecent=splitPercentage
+    )
+    model = LinearDiscriminantAnalysis(solver="lsqr")
+    model.fit(X_train, Y_train)
+    train_pred = model.predict(X_train)
+    test_pred = model.predict(X_test)
+    (train_TP, train_TN, train_FP, train_FN) = loss_calculator(train_pred, Y_train)
+    (test_TP, test_TN, test_FP, test_FN) = loss_calculator(test_pred, Y_test)
+
+    train_acc = accuracy(TP=train_TP, TN=train_TN, FP=train_FP, FN=train_FN)
+    train_recall = recall(TP=train_TP, FN=train_FN)
+    train_precision = precision(TP=train_TP, FP=train_FP)
+    train_f1_measure = f1_measure(precision=train_precision, recall=train_recall)
+
+    test_acc = accuracy(TP=test_TP, TN=test_TN, FP=test_FP, FN=test_FN)
+
+    test_recall = recall(TP=test_TP, FN=test_FN)
+    test_precision = precision(TP=test_TP, FP=test_FP)
+    test_f1_measure = f1_measure(precision=test_precision, recall=test_recall)
+
+    return (
+        train_acc,
+        test_acc,
+        train_recall,
+        test_recall,
+        train_precision,
+        test_precision,
+        train_f1_measure,
+        test_f1_measure,
+    )
+
+
+def DecisionTree(data, label, splitPercentage):
+    (X_train, Y_train, X_test, Y_test) = splitter(
+        data=data, label=label, splitPrecent=splitPercentage
+    )
+    model = DecisionTreeClassifier(
+        # max_depth=5,
+        # max_leaf_nodes=5,
+        criterion="entropy",
+        random_state=RANDOM,
+    )
+    model.fit(X_train, Y_train)
+    train_pred = model.predict(X_train)
+    test_pred = model.predict(X_test)
+    (train_TP, train_TN, train_FP, train_FN) = loss_calculator(train_pred, Y_train)
+    (test_TP, test_TN, test_FP, test_FN) = loss_calculator(test_pred, Y_test)
+
+    train_acc = accuracy(TP=train_TP, TN=train_TN, FP=train_FP, FN=train_FN)
+    train_recall = recall(TP=train_TP, FN=train_FN)
+    train_precision = precision(TP=train_TP, FP=train_FP)
+    train_f1_measure = f1_measure(precision=train_precision, recall=train_recall)
+
+    test_acc = accuracy(TP=test_TP, TN=test_TN, FP=test_FP, FN=test_FN)
+
+    test_recall = recall(TP=test_TP, FN=test_FN)
+    test_precision = precision(TP=test_TP, FP=test_FP)
+    test_f1_measure = f1_measure(precision=test_precision, recall=test_recall)
+
+    return (
+        train_acc,
+        test_acc,
+        train_recall,
+        test_recall,
+        train_precision,
+        test_precision,
+        train_f1_measure,
+        test_f1_measure,
+    )
